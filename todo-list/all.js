f4ef1b9cadc88e475b06375c719e2aa5aba493e5
圈圈 <vvvvvvii@yahoo.com.tw>
2021-01-29 10:53:56 +0800

    完成儲存與刪除本地端功能

--- todo-list/all.js
+++ todo-list/all.js
@@ -6,6 +6,7 @@ const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
 //event listeners
+document.addEventListener('DOMContentLoaded',getTodos); //網頁載好時，執行 getTodos function
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
 filterOption.addEventListener('click',filterTodo);
@@ -33,16 +34,21 @@ function addTodo(event){
     newTodo.innerText = todoInput.value; //輸入什麼就呈現什麼
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo); //把 newTodo 放到 todoDiv 的下方
+    //add todo to localstorage
+    saveLocalTodos(todoInput.value);
+
     //check mark btn
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add('complete-btn');
     todoDiv.appendChild(completedButton); //把 completedButton 放到 todoDiv 的下方    
+    
     //check trash btn
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton); //把 trashButton 放到 todoDiv 的下方    
+    
     //把 todoDiv 放進 todoList 裡
     todoList.appendChild(todoDiv);
     //輸入並加到下方後，自動清空輸入欄位，讓使用者可以再輸入其他待辦事項
@@ -55,6 +61,7 @@ function deleteCheck(e){
     if(item.classList[0] === 'trash-btn'){ //item 的第一層 class 為 trash-btn 時
         const todo = item.parentElement; //若直接打 item.remove() 移除的會是刪除鈕本身，因此須回到父層再刪除
         todo.classList.add("fall"); //在這邊加一個動畫效果，並用css設定動畫效果細節
+        removeLocalTodos(todo); //執行 removeLocalTodos 函式，讓本地端儲存的資料一併刪除
         todo.addEventListener('transitionend',function(){ //動畫效果結束後，才執行以下函式將 todo 本身移除
             todo.remove();
         });
@@ -89,4 +96,80 @@ function filterTodo(e){
                 break;
         }
     });
+}
+//當我們 add todo 時，讓 todo 項目也存到本地端
+function saveLocalTodos(todo){
+    //確認本地端有沒有已存在的todo
+    let todos;
+    //若還沒有，建立空陣列
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    //若本地端已有，用 json 把已存在的 todos 拿來，並建立內容相同的陣列
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    //不論剛剛有沒有拿到東西，把參數 push 進 todos 陣列
+    todos.push(todo); 
+    //將 todos 資料轉回字串，更新到資料庫中
+    localStorage.setItem('todos',JSON.stringify(todos));
+}
+
+//叫出存在本地端的 todos
+function getTodos(){
+    //確認本地端有沒有已存在的todo
+    let todos;
+    //若還沒有，建立空陣列
+    if(localStorage.getItem('todos') === null){
+    todos = [];
+    //若本地端已有，用 json 把已存在的 todos 拿來，並建立內容相同的陣列
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add("todo");
+
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo; //讓文字等於todo這個參數
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo); 
+
+         const completedButton = document.createElement('button');
+         completedButton.innerHTML = '<i class="fas fa-check"></i>';
+         completedButton.classList.add('complete-btn');
+         todoDiv.appendChild(completedButton);     
+    
+         const trashButton = document.createElement('button');
+         trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+         trashButton.classList.add('trash-btn');
+        todoDiv.appendChild(trashButton);   
+    
+        todoList.appendChild(todoDiv);
+    });   
+}
+
+function removeLocalTodos(todo){
+    //確認本地端有沒有已存在的todo
+    let todos;
+    //若還沒有，建立空陣列
+    if(localStorage.getItem('todos') === null){
+    todos = [];
+    //若本地端已有，用 json 把已存在的 todos 拿來，並建立內容相同的陣列
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    /*
+    console.log(todo); //藉由這句知道，當我們點擊刪除按鈕， todo 這個參數對應到的是 .todo fall 這個 div
+    console.log(todo.children); //todo.children 這個參數對應到的是一個陣列，包含：li.todo-item、button.complete-btn、button.trash-btn，裡面又各自包了很多東西，可以發現我們要刪除的項目資料放在 todoitem 的 innerText / innerHTML 底下
+    console.log(todo.children[0].innerText); //再次確認，用 children[0].innerText 的確能拿到要刪除的資料
+    console.log(todos.indexOf('real')); //這個詞在陣列上是第幾個位置
+    */
+
+    //把 todo 的文字叫出來，找到他在陣列上是第幾個位置，然後用 slice 把它切掉
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex),1); //1 的位置要填的數字，代表要刪幾個，只刪一個所以填一
+
+    //最後，把結果傳回本地端
+    localStorage.setItem("todos",JSON.stringify(todos));
 }
