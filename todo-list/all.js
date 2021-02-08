25e6bdfc0a0a5496fc54292dd8415f5950cf5d59
圈圈 <vvvvvvii@yahoo.com.tw>
2021-02-08 12:08:27 +0800

    小修正 modal 樣式

--- todo-list/all.js
+++ todo-list/all.js
@@ -10,7 +10,6 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
 //selectors
 const hamburgerMenu = document.querySelector('#hamburger-menu-icon');
 const hamburgerExit = document.querySelector('#hamburger-menu-exit');
@@ -405,6 +404,7 @@ function analyzeSort(){
     let todos;
     todos = JSON.parse(localStorage.getItem('todos'));
     if(localStorage.getItem('todos') === null){
+        const modalContent = document.querySelector('.modal-content');
         modalContent.innerHTML = '暫無待辦事項';
     }else{
         let todoSortArray = [];
