6c7c8bedfcaefb212d8beef002796c9edbd0e7ad
圈圈 <vvvvvvii@yahoo.com.tw>
2021-05-06 11:04:04 +0800

    把資料架構優化成物件、新增local storage 功能

--- schedule/all.js
+++ schedule/all.js
@@ -7,19 +7,33 @@ const content = document.querySelector('#content');
 const submit = document.querySelector('#submit');
 const subtotal = document.querySelector('#subtotal');
 
-let data=[];
-let totalTime = 0;
-let cheerUpStr = '';
+/*let data={
+    list: [],
+    totalTime : 0,
+    cheerUpStr : ''
+}*/
+let data={};
 
 //functions
 function init(){
-  doneList.innerHTML = '';
-  data.forEach(item=>{
+    doneList.innerHTML = '';
+    if(JSON.parse(localStorage.getItem('doneList'))!=null){
+        //如果 local storage 有東西， data 是 local storage 裡的東西
+        data = JSON.parse(localStorage.getItem('doneList'));
+    }else{
+        //如果 local storage 沒東西，新增空白 data
+        data={
+            list: [],
+            totalTime : 0,
+            cheerUpStr : ''
+        }
+  }
+  data.list.forEach(item=>{
     doneList.innerHTML += `<li class="doneItem">${item.startHour}:${item.startMin} ~ ${item.endHour}:${item.endMin} ${item.content}</li>`
   })
   subtotal.innerHTML = `
-    <h2>今天總共工作了 ${totalTime/60} 小時</h2>
-    <p>${cheerUpStr}</p>
+    <h2>今天總共工作了 ${(data.totalTime/60).toFixed(1)} 小時</h2>
+    <p>${data.cheerUpStr}</p>
   `
 }
 function resetInput(){
@@ -44,24 +58,28 @@ function submitNewItem(e){
   newItem.endHour = Number(endHour.value);
   newItem.endMin = Number(endMin.value);
   newItem.content = content.value;
-  data.push(newItem);
+  data.list.push(newItem);
   resetInput(); //清空表單
   calculateTime(newItem);
+  saveLocalStorage();
   init(); //初始化
 }
 function calculateTime(newItem){
-  totalTime+= 60*(newItem.endHour - newItem.startHour)+(newItem.endMin - newItem.startMin);
-  if(totalTime/60<=4){
-    cheerUpStr = '是不是對自己太好了呢？加油加油！';
-  }else if(totalTime/60<=6){
-    cheerUpStr = '照著這個步調，你一定可以的！';
-  }else if(totalTime/60<=8){
-    cheerUpStr = '哇！今天的工作時數達標啦！';
+  data.totalTime+= 60*(newItem.endHour - newItem.startHour)+(newItem.endMin - newItem.startMin);
+  if(data.totalTime/60<=4){
+    data.cheerUpStr = '是不是對自己太好了呢？加油加油！';
+  }else if(data.totalTime/60<=6){
+    data.cheerUpStr = '照著這個步調，你一定可以的！';
+  }else if(data.totalTime/60<=8){
+    data.cheerUpStr = '哇！今天的工作時數達標啦！';
   }else{
-    cheerUpStr = '好像有點努力過頭了～今天辛苦了，早點休息唷！';
+    data.cheerUpStr = '好像有點努力過頭了～今天辛苦了，早點休息唷！';
   }
+}
+function saveLocalStorage(){
+    localStorage.setItem('doneList',JSON.stringify(data));
 }
 //event Listener
 submit.addEventListener('click',submitNewItem)
+window.addEventListener('load',init);
 
-init();
