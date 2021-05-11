da1a5b6f425cc43835f8d40d6af55a34478e159a
圈圈 <vvvvvvii@yahoo.com.tw>
2021-05-11 12:56:41 +0800

    完成自動計時並能被資料庫儲存功能

--- schedule/all.js
+++ schedule/all.js
@@ -4,17 +4,18 @@ const doneList = document.querySelector('#doneList');
 const timing = document.querySelector('.timing');
 const nowTime = document.querySelector('#nowTime');
 const timeInput = document.querySelectorAll('.timeInput');
-const startHour = document.querySelector('#startHour');
-const startMin = document.querySelector('#startMin');
-const endHour = document.querySelector('#endHour');
-const endMin = document.querySelector('#endMin');
+const startHourInput = document.querySelector('#startHour');
+const startMinInput = document.querySelector('#startMin');
+const endHourInput = document.querySelector('#endHour');
+const endMinInput = document.querySelector('#endMin');
 const contentInput = document.querySelectorAll('.contentInput');
 const submit = document.querySelector('.submit');
 const subtotal = document.querySelector('#subtotal');
 const deleteAllBtn = document.querySelectorAll('.deleteAllBtn');
 
 let data = {};
-let countTimeNumOfClick = 0;
+let countTimeStart = false; // 自動計時是否開始
+let countstartTimeArr = [];
 
 // functions
 function loadingEffect() {
@@ -48,32 +49,12 @@ function init() {
   subtotal.innerHTML = data.totalTime === 0 ? '<h2 class="fz-lg mb-2">趕緊開始紀錄今天的工作吧！</h2>' : `<h2 class="fz-lg mb-2">今天總共工作了 ${(data.totalTime / 60).toFixed(1)} 小時</h2> <p>${data.cheerUpStr}</p>`;
 }
 function resetInput() {
-  startHour.value = '';
-  startMin.value = '';
-  endHour.value = '';
-  endMin.value = '';
+  startHourInput.value = '';
+  startMinInput.value = '';
+  endHourInput.value = '';
+  endMinInput.value = '';
+  contentInput[0].value = '';
   contentInput[1].value = '';
-}
-function countTime() {
-  nowTime.innerHTML = '';
-  const time = new Date();
-  const hours = time.getHours();
-  const mins = time.getMinutes();
-  const secs = time.getSeconds();
-  nowTime.innerHTML = `${hours}:${mins}:${secs}`;
-  nowTime.classList.add('text-center');
-}
-function countLoop() {
-  if (countTimeNumOfClick % 2) {
-    countTime();
-    requestAnimationFrame(countLoop);
-  } else {
-    cancelAnimationFrame(countLoop);
-  }
-}
-function countNumOfClick() { // 不能放在 countLoop() 因為迴圈會重複加 countTimeNumOfClick 導致報錯
-  countTimeNumOfClick += 1;
-  countLoop();
 }
 function calculateTime(newItem) {
   data.totalTime += 60 * (newItem.endHour - newItem.startHour) + (newItem.endMin - newItem.startMin);
@@ -90,44 +71,90 @@ function calculateTime(newItem) {
 function saveLocalStorage() {
   localStorage.setItem('doneList', JSON.stringify(data));
 }
-function submitNewItem(e) {
-  e.preventDefault();
-  const startHourValue = Number(startHour.value);
-  const startMinValue = Number(startMin.value);
-  const endHourValue = Number(endHour.value);
-  const endMinValue = Number(endMin.value);
-  if (startHourValue > 23 || endHourValue > 23 || startMinValue > 59 || endMinValue > 59 || startHour.value === '' || endHour.value === '' || startMin.value === '' || endMin.value === '' || (startHourValue === endHourValue && startMinValue === endMinValue) || startHourValue > endHourValue || (startHourValue === endHourValue && startMinValue > endMinValue)) {
-    // 測試是否為空值的判斷式，要用沒被 Number() 轉過的，不然 0 會被判斷成空值
-    alert('請輸入正確的時間');
-    return;
-  }
-  if (contentInput[1].value === '') {
-    alert('請填入完成事項');
-    return;
-  }
-  const newItem = {};
-  newItem.startHour = startHourValue;
-  newItem.startMin = startMinValue;
-  newItem.endHour = endHourValue;
-  newItem.endMin = endMinValue;
-  newItem.content = contentInput[1].value;
-  data.list.push(newItem);
-  resetInput(); // 清空表單
-  calculateTime(newItem);
-  saveLocalStorage();
-  init(); // 初始化
-}
 function clearLocalStorage() {
   if (window.confirm('是否確定開始新的一天？舊的紀錄會跟喝下孟婆湯一樣回不來ㄛ！')) {
     localStorage.clear(); // local storage 資料歸 0
     init();
   }
 }
+function submitNewItem(e) {
+  const newItem = {};
+  let startHourValue;
+  let startMinValue;
+  let endHourValue;
+  let endMinValue;
+  if (e === 'submit') {
+    startHourValue = Number(startHourInput.value);
+    startMinValue = Number(startMinInput.value);
+    endHourValue = Number(endHourInput.value);
+    endMinValue = Number(endMinInput.value);
+    if (startHourValue > 23 || endHourValue > 23 || startMinValue > 59 || endMinValue > 59 || startHourInput.value === '' || endHourInput.value === '' || startMinInput.value === '' || endMinInput.value === '' || (startHourValue === endHourValue && startMinValue === endMinValue) || startHourValue > endHourValue || (startHourValue === endHourValue && startMinValue > endMinValue)) {
+      // 測試是否為空值的判斷式，要用沒被 Number() 轉過的，不然 0 會被判斷成空值
+      alert('請輸入正確的時間');
+      return;
+    }
+    if (contentInput[1].value === '') {
+      alert('請填入完成事項');
+      return;
+    }
+    newItem.content = contentInput[1].value;
+  } else {
+    const startTime = nowTime.innerHTML.split(' ~ ')[0];
+    const endTime = nowTime.innerHTML.split(' ~ ')[1];
+    startHourValue = Number(startTime.split(':')[0]);
+    startMinValue = Number(startTime.split(':')[1]);
+    endHourValue = Number(endTime.split(':')[0]);
+    endMinValue = Number(endTime.split(':')[1]);
+    newItem.content = contentInput[0].value;
+  }
+  newItem.startHour = startHourValue;
+  newItem.startMin = startMinValue;
+  newItem.endHour = endHourValue;
+  newItem.endMin = endMinValue;
+  data.list.push(newItem);
+  resetInput(); // 清空表單
+  nowTime.innerHTML = '尚未開始計時'; // 確定結束時間被紀錄後，調回預設文字
+  calculateTime(newItem);
+  saveLocalStorage();
+  init(); // 初始化
+}
+function countLoop() {
+  if (countTimeStart) {
+    const time = new Date();
+    const endHours = time.getHours();
+    const endMins = time.getMinutes();
+    const endSecs = time.getSeconds();
+    nowTime.innerHTML = `${countstartTimeArr[0]}:${countstartTimeArr[1]}:${countstartTimeArr[2]} ~ ${endHours}:${endMins}:${endSecs}`;
+    requestAnimationFrame(countLoop);
+  } else {
+    cancelAnimationFrame(countLoop);
+  }
+}
+function countTimeStatus() { // 不能放在 countLoop() 會計算錯誤
+  countTimeStart = !countTimeStart;
+  if (countTimeStart) {
+    nowTime.innerHTML = '';
+    countstartTimeArr = [];
+    const time = new Date();
+    countstartTimeArr.push(time.getHours(), time.getMinutes(), time.getSeconds());
+    countLoop();
+  } else {
+    submitNewItem('timing');
+  }
+}
+function checkBtnType(e) {
+  e.preventDefault();
+  if (e.target.dataset.btn === 'submit') {
+    submitNewItem('submit');
+  } else if (e.target.dataset.btn === 'timing') {
+    countTimeStatus();
+  }
+}
 function inputFocusEffect(e) {
   if (e.keyCode === 13) {
     let nextItem;
     if (e.target.dataset.num < 4) { // 按 enter 且不是停在最後一個 timeInput 時
-      nextItem = timeInput[e.target.dataset.num]; // 下一個物件在 timeInput 陣列的 index 位置為 e.target.dataset.num
+      nextItem = timeInput[e.target.dataset.num]; // 下個物件在 timeInput 陣列的 index 為e.target.dataset.num
     } else if (e.target.dataset.num === 4) { // 點到最後一個 timeInput 要跳 contentInput
       nextItem = contentInput[1];
     } else {
@@ -136,11 +163,12 @@ function inputFocusEffect(e) {
     nextItem.focus(); // HTMLElement.focus() 讓特定 element 被 focus
   }
 }
+
 // event Listener
 window.addEventListener('DOMContentLoaded', loadingEffect); // loader effect
 window.addEventListener('load', init);
-timing.addEventListener('click', countNumOfClick);
-submit.addEventListener('click', submitNewItem);
+timing.addEventListener('click', checkBtnType);
+submit.addEventListener('click', checkBtnType);
 deleteAllBtn.forEach((item) => item.addEventListener('click', clearLocalStorage));
 timeInput.forEach((item) => item.addEventListener('keydown', inputFocusEffect)); // input 點 enter 會自動跳下一個 input
 contentInput.forEach((item) => item.addEventListener('keydown', inputFocusEffect)); // contentinput 點 enter 會自動跳送出
